5de801f18e5daf05ee0545e210866c41f8ec7c14
Xavier Aguilar <kevinjkevps4@gmail.com>
2022-06-27 16:29:50 -0500

    Configuraciones del log

--- src/main/java/com/uce/edu/demo/ProyectoU2KaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2KaApplication.java
@@ -3,19 +3,23 @@ package com.uce.edu.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.log4j.Logger;
 
 @SpringBootApplication
 public class ProyectoU2KaApplication implements CommandLineRunner{
-
+	
+	private static final Logger log = Logger.getRootLogger();
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2KaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		//configuracion de logs// log4j
+		System.out.println("---------Consola normal-------------");
 		System.out.println("hola mundo");
+		System.out.println("---------Consola LOG-------------");
+		log.info("Hola mundo");
 	}
 
 }
