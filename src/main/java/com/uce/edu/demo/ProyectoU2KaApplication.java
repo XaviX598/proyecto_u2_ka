66a41db5ec5c0bfd6ee276263dd0e50247a20b7c
Xavier Aguilar <kevinjkevps4@gmail.com>
2022-07-19 20:06:48 -0500

    taller24 relaciones de uno a uno

--- src/main/java/com/uce/edu/demo/ProyectoU2KaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2KaApplication.java
@@ -1,6 +1,6 @@
 package com.uce.edu.demo;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.estudiante.repository.modelo.Estudiante;
-import com.uce.edu.demo.estudiante.repository.modelo.EstudianteContadoEdadApellido;
-import com.uce.edu.demo.estudiante.repository.modelo.EstudianteSencillo;
-import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
-import com.uce.edu.demo.repository.modelo.PersonaContadorGenero;
-import com.uce.edu.demo.repository.modelo.PersonaSencilla;
-import com.uce.edu.demo.service.IPersonaJpaService;
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
+import com.uce.edu.demo.service.ICiudadanoService;
 
 //import org.slf4j.LoggerFactory;
 //import org.slf4j.Logger;
@@ -24,11 +20,12 @@ public class ProyectoU2KaApplication implements CommandLineRunner {
 
 	private static final Logger log = Logger.getRootLogger();
 
+	
 	@Autowired
-	private IPersonaJpaService iPersonaJpaService;
-
+	private ICiudadanoService iCiudadanoService;
+	
 	@Autowired
-	private IEstudianteJpaService iEstudianteJpaService;
+	//private IEmpleadoService iEmpleadoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2KaApplication.class, args);
@@ -36,121 +33,33 @@ public class ProyectoU2KaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-//		//jpa buscar
-//		
-		// log.info("Dato con JPA: " + this.iPersonaJpaService.buscar(7));
-//		
-//		//jpa insertar
-		// Persona per = new Persona();
-//		// debido a secuancia no hace falta id
-//		// per.setId(7);
-//		per.setNombre("Rom");
-//		per.setApellido("Aguilar");
-//		per.setGenero("F");
-//		per.setCedula("451234");
-		// this.iPersonaJpaService.guardar(per);
-
-		// actualizar por apellido
-//		int resultado=this.iPersonaJpaService.actualizarPorApellido("Femenino", "Matamoros");
-//		log.info("Cantidad de registros actualizados: "+resultado);
-
-		// eliminar por genero
-//		int resultado1 = this.iPersonaJpaService.eliminarPorGenero("M");
-//		log.info("Cantidad de registros eliminados: " + resultado1);
-
-//		
-//		
-//		//buscar por cedula
-//		Persona Per1 =  this.iPersonaJpaService.buscarPorCedula("545165454");
-//		//log.info("Persona encontrada: "+Per1);
-//		
-//		//buscar por apellido
-//		List<Persona> listaPersona = this.iPersonaJpaService.buscarPorApellido("Matamoros") ;
-//		for(Persona item : listaPersona) {
-//			log.info("Personas buscadas por apellido: "+ item );
-//		}
-//		
-//		//buscar por apellido
-//		List<Persona> listaPersona1 = this.iPersonaJpaService.buscarPorGenero("F") ;
-//		for(Persona item : listaPersona1) {
-//			log.info("Personas buscadas por genero: "+ item );
-//		}
-//		
-//		//buscar por apellido
-//		List<Persona> listaPersona2 = this.iPersonaJpaService.buscarPorNombre("Rom") ;
-//		for(Persona item : listaPersona2) {
-//			log.info("Personas buscadas por nombre: "+ item );
-//		}
-//		
-
-//		//actualizar jpa
-//		per.setId(7);
-//		per.setNombre("Victoria");
-//		per.setApellido("Velasco");
-//		 this.iPersonaJpaService.actualizar(per);
-//		
-//		//eliminar jpa
-		// this.iPersonaJpaService.eliminar(4);
-
-		// buscar por cedula TypedQuery
-//		Persona PerTyped = this.iPersonaJpaService.buscarPorCedulaTyped("8884541");
-//		log.info("Persona encontrada con typed: " + PerTyped);
-//
-//		// buscar por cedula NamedQuery
-//		Persona PerNamed = this.iPersonaJpaService.buscarPorCedulaNamed("7848484");
-//		log.info("Persona encontrada con Named: " + PerNamed);
-//		// buscar por cedula TypedQuery y NamedQuery
-//		Persona PerTypedNamed = this.iPersonaJpaService.buscarPorCedulaTypedNamed("45612315");
-//		log.info("Persona encontrada con TypedQuery y NamedQuery: " + PerTypedNamed);
-//		// buscar por nombre y apellido
-//		
-//		List<Persona> listaPer= this.iPersonaJpaService.buscarPorNombreApellido("Rom", "Aguilar");
-//		for(Persona item : listaPer) {
-//			log.info("Personas buscadas por nombre y apellido: "+ item );
-
-//		// buscar por cedula Native
-//		Persona personaNative = this.iPersonaJpaService.buscarPorCedulaNative("451234");
-//		log.info("Persona encontrada con Native Query: " + personaNative);
-//
-//		// buscar por cedula Named Native
-//		Persona personaNamedNative = this.iPersonaJpaService.buscarPorCedulaNamedNative("451234");
-//		log.info("Persona encontrada con Named Native Query: " + personaNamedNative);
-
-//		// buscar por cedula Criteria Api
-//		Persona personaCriteriaApi = this.iPersonaJpaService.buscarPorCedulaCriteriaApi("45612315");
-//		log.info("Persona encontrada con Crtiteria API: " + personaCriteriaApi);
-//
-//		// buscar por cedula Criteria Api
-//		Persona dinamicamenteCriteriaApi = this.iPersonaJpaService.buscarDinamicamente("Edison","Cayambe","M");
-//		log.info("Persona encontrada dinamicamente con Crtiteria API: " + dinamicamenteCriteriaApi);
-
-////		buscar por apellido sencillo
-//		List<PersonaSencilla> listaPersonaSencilla = this.iPersonaJpaService.buscarPorApellidoSenciloo("Aguilar");
-//		for (PersonaSencilla perItem : listaPersonaSencilla) {
-//			log.info("Perona encontrada mediante PersonaSencilla: " + perItem);
-//		}
-//
-////		consultar numero de personaspor genero
-//		List<PersonaContadorGenero> listaPersonaGenero = this.iPersonaJpaService.consultarCantidadPorGenero();
-//		for (PersonaContadorGenero perItem : listaPersonaGenero) {
-//			log.info("Genero: " + perItem);
-//		}
 		
-		//buscar por apellido sencillo
-		List<EstudianteSencillo> listaEstudianteSencilla = this.iEstudianteJpaService.busquedaApellidoCorreo( "Aguilar");
-		for (EstudianteSencillo perItem : listaEstudianteSencilla) {
-			log.info("Estudiante encontrada mediante EstudianteSencillo: " + perItem);
-		}
-
-		//consultar numero de estudiantes con apellido
-		List<EstudianteContadoEdadApellido> listaEstudianteEdadApellido = this.iEstudianteJpaService.consultarEdadConApellido("Aguilar");
-		log.info("Con el apellido ingresados las edades son");
-		for (EstudianteContadoEdadApellido perItem : listaEstudianteEdadApellido) {
-			log.info( "Edad: " + perItem);
-		}
+		Ciudadano ciu1 = new Ciudadano();
+		ciu1.setNombre("David");
+		ciu1.setApellido("Aguilar");
+		
+		Empleado empl1 = new Empleado();
+		empl1.setCodigoIess("5451");
+		empl1.setSalario(new BigDecimal(50));
+		empl1.setCiudadano(ciu1);
+		
+		ciu1.setEmpleado(empl1);
+		
+		this.iCiudadanoService.insertar(ciu1);
+		
+//		Ciudadano ciu2 = new Ciudadano();
+//		ciu2.setNombre("Davi2d");
+//		ciu2.setApellido("Aguilar2");
+//		
+//		Empleado empl2 = new Empleado();
+//		empl2.setCodigoIess("54512");
+//		empl2.setSalario(new BigDecimal(502));
+//		empl2.setCiudadano(ciu2);
+//		
+//		ciu2.setEmpleado(empl2);
+//		this.iEmpleadoService.insertar(empl2);
 		
 		
 	}
 }
-//}
+
---
+++ src/main/java/com/uce/edu/demo/service/EmpleadoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.uce.edu.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uce.edu.demo.repository.IEmpleadoRepository;
+import com.uce.edu.demo.repository.modelo.Empleado;
+//@Service
+public class EmpleadoServiceImpl implements IEmpleadoService{
+//	@Autowired
+//	private IEmpleadoRepository iEmpleadoRepository;
+//
+//	@Override
+//	public void insertar(Empleado e) {
+//		// TODO Auto-generated method stub
+//		this.iEmpleadoRepository.insertar(e);
+//	}
+
+}
